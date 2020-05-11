b86fac3135bffc087066bac6681f4cf7a205b54e
sfgardev <sf.garol@gmail.com>
2020-05-11 16:32:49 +0500

    add totop btn

--- app/js/main.js
+++ app/js/main.js
@@ -61,4 +61,21 @@ $(function(){
     return false;
   });
 
+  var btn = $("#top-btn");
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass("show");
+    } else {
+      btn.removeClass("show");
+    }
+  });
+
+  btn.on("click", function(event) {
+    event.preventDefault();
+    $("html, body").animate({
+      scrollTop:0
+    }, 500);
+  });
+
 });
